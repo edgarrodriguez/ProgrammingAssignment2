8d4bfa26bab10435f4c39c2496470c5afa2e3c6e
Edgar Rodriguez <edgarr@outlook.com>
2014-05-25 12:28:05 -0500

    Final File Containing Special Matrix with Inverse and Vector Example

--- cachematrix.R
+++ cachematrix.R
@@ -7,12 +7,12 @@
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y               #set the value of special matrix that is kept with its inverse
     m <<- NULL
   }
   get <- function() x
   setinv <- function(solve) m <<- solve
-  getinv <- function() m
+  getinv <- function() m  #retrieve the value of special matrix (calculate or retrieve from cache)
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -23,13 +23,43 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()
+  m <- x$getinv() #get the inverse of the matrix x
+  if(!is.null(m)) {
+    message("getting cached data")  
+    return(m)     #If the variable m is not empty, use cached inverse
+  }
+  data <- x$get() #If the variable m is empty, calculate the inverse
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
+  m <- mean(data, ...)
+  x$setmean(m)
   m
 }
+
+
+
